9d7dbbc4db352914c0311895d532980dacb3992d
unknown <victoroliveira9128@digitalcollege.internal>
2026-07-17 19:16:08 -0300

    feat: implementar servidor basico

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,19 @@
-import http from 'node:http';
+import express from 'express';
+const app = express();
+app.use(express.json());
+app.get('/customers', (request, response) => {
+    const customers = [{
+        name:'Victor',
+        status:true
+    },{
+         name:'Karol',
+         status:true
+    },{
+         name:'Rafael',
+         status:true
+    }];
+
+    response.status(200).json(customers);
+});
+
+app.listen(3000);
