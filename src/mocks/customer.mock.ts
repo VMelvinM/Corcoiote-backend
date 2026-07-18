71309b0b0dc7aefdd808b612dedb145356cc8a60
unknown <victoroliveira9128@digitalcollege.internal>
2026-07-17 21:40:38 -0300

    refactor : mover dados similares para diretorio dedicado

---
+++ src/mocks/customer.mock.ts
@@ -0,0 +1,18 @@
+import type { Customer } from "../types.ts";
+
+const customers: Customer[] = [{
+    id: 1,
+    name: 'Victor',
+    status: true
+}, {
+    id: 2,
+    name: 'Karol',
+    status: true
+}, {
+    id: 3,
+    name: 'Rafael',
+    status: true
+}];
+
+
+export default customers;
